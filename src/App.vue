2e1df1ef15e84baf507ced98bffb715bf83566e1
jiangmaniu <jiangmaniu@qq.com>
2022-08-02 10:40:41 +0800

    chore: 复现 BUG

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
+
+import type { Ref } from 'vue'
+
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
   title: 'Vitesse',
@@ -19,6 +22,27 @@ useHead({
     },
   ],
 })
+
+// 从 hooks 工具函数返回的 ref 变量
+const refDataA = ref(1)
+const refDataB = ref(2)
+setInterval(() => { refDataA.value++; refDataB.value *= 2 }, 1000)
+
+// 在业务中使用
+const $refDataA = $(refDataA)
+const $refDataB = $(refDataB)
+
+// 类似hooks的use工具函数
+function useAdd(...params: Ref<number>[]) {
+  return computed(() => params.reduce((acc, param) => { return acc + param.value }, 0))
+}
+
+// 需要使用 xxx as unknown as number 断言
+// 否则将被视为 JQuery<Ref<number>> 类型
+const afterComputed = useAdd($$($refDataA as unknown as number), $$($refDataB as unknown as number))
+
+// 持续输出
+watch(afterComputed, () => console.log(afterComputed.value))
 </script>
 
 <template>
